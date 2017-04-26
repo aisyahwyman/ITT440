d1059ad1f2abd53a7012a18337ad74b8e0d62ed5
aisyahwyman <chaawyman95@gmail.com>
2017-04-26 12:21:59 +0800

    Add files via upload

--- client.lua
+++ client.lua
@@ -1,14 +1,22 @@
-local host, port = "172.168.22.16",4969
 local socket = require("socket")
-local tcp = assert(socket.tcp())
-
-tcp:connect(host, port);
---note the newline below
-tcp:send("hello world\n");
-
-while true do
-    local s, status, partial = tcp:receive()
-    print(s or partial)
-    if status == "closed" then break end
+host = host or "192.168.179.1"
+port = port or 8080
+if arg then
+	host = arg[1] or host
+	port = arg[2] or port
 end
-tcp:close()
+print("Attempting connection to host '" ..host.. "' and port " ..port.. "...")
+c = assert(socket.connect(host, port))
+print("Connected! Please type stuff (empty line to stop):")
+l = io.read()
+while l and l ~= "" and not e do
+	assert(c:send(l .. "\n"))
+
+	msg, err = c:receive()
+	if not err then
+		print("Reversed Message: " .. msg)
+
+	end
+
+	l = io.read()
+end
