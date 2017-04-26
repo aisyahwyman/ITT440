1d84c243b6b279c523f612a3a92a10b837622ca1
aisyahwyman <chaawyman95@gmail.com>
2017-04-26 13:40:41 +0800

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,16 +1,16 @@
 local socket = require("socket")
-host = host or "192.168.179.1"
+host = host or "202.58.86.29"
 port = port or 8080
 if arg then
 	host = arg[1] or host
 	port = arg[2] or port
 end
 print("Binding to host '" ..host.. "' and port " ..port.. "...")
-s = assert(socket.bind(host, port))
-i, p   = s:getsockname()
-assert(i, p)
+local server = assert(socket.bind("*", 0))
+local ip, ports = server:getsockname()
+assert(ip, ports)
 print("Waiting connection from talker on " .. i .. ":" .. p .. "...")
-c = assert(s:accept())
+c = assert(server:accept())
 print("Connected. Here is the stuff:")
 l, err = c:receive()
 while not err do
