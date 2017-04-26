5756d4a3b8c47acdcf83d1c62e66726839a83f3c
aisyahwyman <chaawyman95@gmail.com>
2017-04-26 13:46:07 +0800

    Update socketserver.lua

--- socketserver.lua
+++ socketserver.lua
@@ -1,17 +1,11 @@
--- load namespace
 local socket = require("socket")
-
--- create a TCP socket and bind it to the local host, at any port
 local server = assert(socket.bind("*", 0))
-
--- find out which port the OS chose for us
 local ip, port = server:getsockname()
 
 -- print a message informing what's up
 print("Please telnet to localhost on port " .. port)
 print("After connecting, you have 10s to enter a line to be echoed")
 
--- loop forever waiting for clients
 while 1 do
 
   -- wait for a connection from any client
